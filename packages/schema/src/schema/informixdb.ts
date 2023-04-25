eb908025b8c910d3d4dbd636022c16174c2df1cd
nangelov77 <nikolay.angelov@etisoftware.com>
2023-04-25 14:21:51 +0300

    Fix misidentified primary key issue

--- packages/schema/src/schema/informixdb.ts
+++ packages/schema/src/schema/informixdb.ts
@@ -45,11 +45,6 @@ type RawColumn = {
 };
 
 export function rawColumnToColumn(rawColumn: RawColumn): Column {
-	// let dataType = rawColumn.column_type.replace(/\(.*?\)/, '');
-	// if (rawColumn.column_type.startsWith('tinyint(1)')) {
-	// 	dataType = 'boolean';
-	// }
-
 	return {
 		name: rawColumn.column_name,
 		table: rawColumn.table_name,
@@ -170,7 +165,7 @@ export default class InformixDB implements SchemaInspector {
 	columnInfo(table: string): Promise<Column[]>;
 	columnInfo(table: string, column: string): Promise<Column>;
 	async columnInfo(table?: string, column?: string) {
-		const storedProcedure = `
+		const spAllInfo = `
 		DROP FUNCTION IF EXISTS sp_allinfo;
 
 		CREATE FUNCTION sp_allinfo() RETURNING
@@ -323,420 +318,104 @@ export default class InformixDB implements SchemaInspector {
 			END FOREACH;
 		END FUNCTION;
 		`;
-		this.knex.raw(storedProcedure);
+		this.knex.raw(spAllInfo);
 
-		// const get_column_default = `
-		// DROP FUNCTION IF EXISTS get_column_default;
+		const isPrimaryKey = `
+		DROP FUNCTION IF EXISTS is_primary_key;
 
-		// CREATE FUNCTION get_column_default(itabname VARCHAR(128), icolname VARCHAR(128)) RETURNING VARCHAR(128) AS cdefault;
+		CREATE FUNCTION is_primary_key(itabname VARCHAR(128), 
+									icolname VARCHAR(128)) 
+			RETURNING INTEGER AS ispri; 
 
-		// DEFINE l_tabname   VARCHAR(128);
-		// DEFINE l_colname   VARCHAR(128);
-		// DEFINE l_coldefault  VARCHAR(128);
+		DEFINE l_dtabname   VARCHAR(128);
+		DEFINE l_dcolname   VARCHAR(128);
+		DEFINE l_colkey     VARCHAR(128);
 
-		// SELECT st.tabname, sc.colname,
-		// CASE
-		// WHEN
-		// 	sc.coltype = 0 OR
-		// 	sc.coltype = 13 OR
-		// 	sc.coltype = 15 OR
-		// 	sc.coltype = 16 OR
-		// 	sc.coltype = 40 OR
-		// 	sc.coltype = 41
-		// THEN sd.default
-		// ELSE second_string(sd.default)
-		// END AS coldefault
-		// INTO l_tabname, l_colname, l_coldefault
-		// FROM sysdefaults sd, syscolumns sc, systables st
-		// WHERE sd.type = 'L' AND sd.tabid = sc.tabid AND sd.colno = sc.colno
-		// AND sd.tabid = st.tabid AND st.tabname = itabname AND sc.colname = icolname;
 
-		// RETURN l_coldefault;
+		FOREACH
+		select st.tabname as table_name, 
+		scol.colname as column_name, 
+		CASE
+			WHEN scol.colname IN 
+			( 
+			select unique
+				(select c.colname from syscolumns c where c.tabid = i.tabid and c.colno = i.part1)
+				from sysindexes i , systables t
+					where i.tabid = t.tabid
+					and t.tabname = st.tabname
+					and idxname in
+			(
+				select c.idxname as pk_idx
+				from sysconstraints c, systables t,
+				outer (sysreferences r, systables t2, sysconstraints c2)
+				where t.tabname = st.tabname
+				and t.tabid = c.tabid
+				and r.constrid = c.constrid
+				and t2.tabid = r.ptabid
+				and c2.constrid = r.constrid
+				AND c.constrtype = 'P'
+			)
+			) THEN 'PRI'
+			WHEN scol.colname IN 
+			( 
+			select unique
+				(select c.colname from syscolumns c where c.tabid = i.tabid and c.colno = i.part2)
+				from sysindexes i , systables t
+					where i.tabid = t.tabid
+					and t.tabname = st.tabname
+					and idxname in
+			(
+				select c.idxname as pk_idx
+				from sysconstraints c, systables t,
+				outer (sysreferences r, systables t2, sysconstraints c2)
+				where t.tabname = st.tabname
+				and t.tabid = c.tabid
+				and r.constrid = c.constrid
+				and t2.tabid = r.ptabid
+				and c2.constrid = r.constrid
+				AND c.constrtype = 'P'
+			)
+			) THEN 'PRI'
+			WHEN scol.colname IN 
+			( 
+			select unique
+				(select c.colname from syscolumns c where c.tabid = i.tabid and c.colno = i.part3)
+				from sysindexes i , systables t
+					where i.tabid = t.tabid
+					and t.tabname = st.tabname
+					and idxname in
+			(
+				select c.idxname as pk_idx
+				from sysconstraints c, systables t,
+				outer (sysreferences r, systables t2, sysconstraints c2)
+				where t.tabname = st.tabname
+				and t.tabid = c.tabid
+				and r.constrid = c.constrid
+				and t2.tabid = r.ptabid
+				and c2.constrid = r.constrid
+				AND c.constrtype = 'P'
+			)
+			) THEN 'PRI'
+		END AS column_key
+		INTO l_dtabname, l_dcolname, l_colkey
+		from systables st, syscolumns scol, outer sysdefaults sdef
+		where st.tabid = scol.tabid
+			and scol.tabid = sdef.tabid and scol.colno = sdef.colno
+			and tabname = itabname
+			and colname = icolname
+		
+		IF l_colkey == 'PRI' THEN
+			RETURN 1;
+		ELSE
+			RETURN 0;
+		END IF;
+		
+		END FOREACH;
 
-		// END FUNCTION;
-		// `;
+		END FUNCTION;
+		`;
 
-		// this.knex.raw(get_column_default);
-
-		// const second_string = `
-		// DROP FUNCTION IF EXISTS second_string;
-
-		// CREATE PROCEDURE second_string(input CHAR(256)) RETURNING CHAR(256);
-
-		// DEFINE var1 CHAR(256);
-		// DEFINE var2 INT;
-		// LET var1 = input;
-		// LET var2 = 1;
-		// 	WHILE var2 = 1
-		// 	IF substr(var1, 0, 1) = ' '
-		// 		THEN RETURN substr(var1, 2);
-		// 	END IF;
-		// 	LET var1 = substr(var1, 2);
-		// 	END WHILE;
-		// END PROCEDURE;
-		// `;
-
-		// this.knex.raw(second_string);
-
-		// const is_primary_key = `
-		// CREATE FUNCTION is_primary_key(itabname VARCHAR(128), icolname VARCHAR(128))
-		// 	RETURNING INTEGER AS ispri;
-
-		// DEFINE l_dtabname   VARCHAR(128);
-		// DEFINE l_dcolname   VARCHAR(128);
-		// DEFINE l_colkey     VARCHAR(128);
-
-		// FOREACH
-		// select st.tabname as table_name,
-		// scol.colname as column_name,
-		// CASE
-		// 	WHEN scol.colname IN
-		// 	(
-		// 	select unique
-		// 		(select c.colname from syscolumns c where c.tabid = i.tabid and c.colno = i.part1)
-		// 		from sysindexes i , systables t
-		// 			where i.tabid = t.tabid
-		// 			and t.tabname = st.tabname
-		// 			and idxname in
-		// 	(
-		// 		select c.idxname as pk_idx
-		// 		from sysconstraints c, systables t,
-		// 		outer (sysreferences r, systables t2, sysconstraints c2)
-		// 		where t.tabname = st.tabname
-		// 		and t.tabid = c.tabid
-		// 		and r.constrid = c.constrid
-		// 		and t2.tabid = r.ptabid
-		// 		and c2.constrid = r.constrid
-		// 		AND c.constrtype = 'P'
-		// 	)
-		// 	) THEN 'PRI'
-		// 	WHEN scol.colname IN
-		// 	(
-		// 	select unique
-		// 		(select c.colname from syscolumns c where c.tabid = i.tabid and c.colno = i.part2)
-		// 		from sysindexes i , systables t
-		// 			where i.tabid = t.tabid
-		// 			and t.tabname = st.tabname
-		// 			and idxname in
-		// 	(
-		// 		select c.idxname as pk_idx
-		// 		from sysconstraints c, systables t,
-		// 		outer (sysreferences r, systables t2, sysconstraints c2)
-		// 		where t.tabname = st.tabname
-		// 		and t.tabid = c.tabid
-		// 		and r.constrid = c.constrid
-		// 		and t2.tabid = r.ptabid
-		// 		and c2.constrid = r.constrid
-		// 		AND c.constrtype = 'P'
-		// 	)
-		// 	) THEN 'PRI'
-		// 	WHEN scol.colname IN
-		// 	(
-		// 	select unique
-		// 		(select c.colname from syscolumns c where c.tabid = i.tabid and c.colno = i.part3)
-		// 		from sysindexes i , systables t
-		// 			where i.tabid = t.tabid
-		// 			and t.tabname = st.tabname
-		// 			and idxname in
-		// 	(
-		// 		select c.idxname as pk_idx
-		// 		from sysconstraints c, systables t,
-		// 		outer (sysreferences r, systables t2, sysconstraints c2)
-		// 		where t.tabname = st.tabname
-		// 		and t.tabid = c.tabid
-		// 		and r.constrid = c.constrid
-		// 		and t2.tabid = r.ptabid
-		// 		and c2.constrid = r.constrid
-		// 		AND c.constrtype = 'P'
-		// 	)
-		// 	) THEN 'PRI'
-		// END AS column_key
-		// INTO l_dtabname, l_dcolname, l_colkey
-		// from systables st, syscolumns scol, outer sysdefaults sdef
-		// where st.tabid = scol.tabid
-		// 	and scol.tabid = sdef.tabid and scol.colno = sdef.colno
-		// 	and tabname = itabname
-		// 	and colname = icolname
-
-		// IF l_colkey == 'PRI' THEN
-		// 	RETURN 1;
-		// ELSE
-		// 	RETURN 0;
-		// END IF
-
-		// END FOREACH;
-
-		// END FUNCTION;
-		// `;
-
-		// this.knex.raw(is_primary_key);
-
-		// const sp_allconst = `
-		// DROP FUNCTION IF EXISTS sp_allinfo;
-
-		// CREATE FUNCTION sp_allinfo() RETURNING VARCHAR(128) AS dtabname,
-		// 									VARCHAR(128) AS dcolname,
-		// 									VARCHAR(128) AS ctabname,
-		// 									VARCHAR(128) AS ccolname,
-		// 									VARCHAR(128) AS cname;
-
-		// 	DEFINE l_constrname VARCHAR(128);
-
-		// 	DEFINE l_dtabname   VARCHAR(128);
-		// 	DEFINE l_ptabname   VARCHAR(128);
-		// 	DEFINE t_tabname    VARCHAR(128);
-
-		// 	DEFINE l_dtabid     LIKE systables.tabid;
-		// 	DEFINE l_ptabid     LIKE systables.tabid;
-		// 	DEFINE t_tabid      LIKE systables.tabid;
-
-		// 	DEFINE l_dindexkeys LIKE sysindices.indexkeys;
-		// 	DEFINE l_pindexkeys LIKE sysindices.indexkeys;
-
-		// 	DEFINE l_dcolno     LIKE syscolumns.colno;
-		// 	DEFINE l_pcolno     LIKE syscolumns.colno;
-		// 	DEFINE t_colno     LIKE syscolumns.colno;
-
-		// 	DEFINE l_dcolname   VARCHAR(128);
-		// 	DEFINE l_pcolname   VARCHAR(128);
-		// 	DEFINE t_colname   VARCHAR(128);
-
-		// 	DEFINE l_keyid      SMALLINT;
-
-		// 	DEFINE l_ccnt       SMALLINT;
-
-		// 	FOREACH
-		// 	SELECT st.tabid, st.tabname, sc.colno, sc.colname
-		// 	INTO t_tabid, t_tabname, t_colno, t_colname
-		// 	FROM systables AS st
-		// 	JOIN syscolumns AS sc ON st.tabid = sc.tabid
-		// 	WHERE   st.tabname not like 'sys%'
-		// 	AND     st.tabname not like 'vw%'
-
-		// 	LET l_ccnt = 0;
-		// 	FOREACH
-
-		// 		SELECT  dc.constrname,
-		// 				TRIM(dt.tabname), dt.tabid, di.indexkeys,
-		// 				TRIM(pt.tabname), pt.tabid, pi.indexkeys
-		// 		INTO    l_constrname,
-		// 				l_dtabname, l_dtabid, l_dindexkeys,
-		// 				l_ptabname, l_ptabid, l_pindexkeys
-		// 		FROM    systables      AS dt
-		// 		LEFT JOIN    sysconstraints AS dc ON dc.tabid    = dt.tabid
-		// 		LEFT JOIN    sysobjstate    AS do ON do.name     = dc.constrname
-		// 		LEFT JOIN    sysindices     AS di ON di.idxname  = dc.idxname
-		// 		LEFT JOIN    sysreferences  AS dr ON dr.constrid = dc.constrid
-		// 		LEFT JOIN    sysconstraints AS pc ON pc.constrid = dr.primary
-		// 		JOIN    systables      AS pt ON pt.tabid    = pc.tabid
-		// 		LEFT JOIN    sysindices     AS pi ON pi.idxname  = pc.idxname
-		// 		WHERE dt.tabid = t_tabid
-
-		// 		LET l_keyid = 0;
-
-		// 		LET l_dcolno = ikeyextractcolno(l_dindexkeys, l_keyid);
-		// 		LET l_pcolno = ikeyextractcolno(l_pindexkeys, l_keyid);
-
-		// 		IF l_dcolno != 0 AND l_dcolno == t_colno THEN
-
-		// 		SELECT  TRIM(colname)
-		// 		INTO    l_dcolname
-		// 		FROM    syscolumns
-		// 		WHERE   tabid = l_dtabid
-		// 		AND     colno = l_dcolno;
-
-		// 		SELECT  TRIM(colname)
-		// 		INTO    l_pcolname
-		// 		FROM    syscolumns
-		// 		WHERE   tabid = l_ptabid
-		// 		AND     colno = l_pcolno;
-
-		// 		IF l_dcolname == t_colname THEN
-		// 			LET l_ccnt = l_ccnt + 1;
-		// 			RETURN l_dtabname, l_dcolname, l_ptabname, l_pcolname, l_constrname WITH RESUME;
-		// 		END IF
-		// 		END IF
-		// 	END FOREACH;
-
-		// 	FOREACH
-
-		// 		SELECT  dc.constrname,
-		// 				TRIM(dt.tabname), dt.tabid, di.indexkeys,
-		// 				TRIM(pt.tabname), pt.tabid, pi.indexkeys
-		// 		INTO    l_constrname,
-		// 				l_dtabname, l_dtabid, l_dindexkeys,
-		// 				l_ptabname, l_ptabid, l_pindexkeys
-		// 		FROM    systables      AS dt
-		// 		LEFT JOIN    sysconstraints AS dc ON dc.tabid    = dt.tabid
-		// 		LEFT JOIN    sysobjstate    AS do ON do.name     = dc.constrname
-		// 		LEFT JOIN    sysindices     AS di ON di.idxname  = dc.idxname
-		// 		LEFT JOIN    sysreferences  AS dr ON dr.constrid = dc.constrid
-		// 		LEFT JOIN    sysconstraints AS pc ON pc.constrid = dr.primary
-		// 		JOIN    systables      AS pt ON pt.tabid    = pc.tabid
-		// 		LEFT JOIN    sysindices     AS pi ON pi.idxname  = pc.idxname
-		// 		WHERE dt.tabid = t_tabid
-
-		// 		LET l_keyid = 1;
-
-		// 		LET l_dcolno = ikeyextractcolno(l_dindexkeys, l_keyid);
-		// 		LET l_pcolno = ikeyextractcolno(l_pindexkeys, l_keyid);
-
-		// 		IF l_dcolno != 0 AND l_dcolno == t_colno THEN
-
-		// 		SELECT  TRIM(colname)
-		// 		INTO    l_dcolname
-		// 		FROM    syscolumns
-		// 		WHERE   tabid = l_dtabid
-		// 		AND     colno = l_dcolno;
-
-		// 		SELECT  TRIM(colname)
-		// 		INTO    l_pcolname
-		// 		FROM    syscolumns
-		// 		WHERE   tabid = l_ptabid
-		// 		AND     colno = l_pcolno;
-
-		// 		IF l_dcolname == t_colname THEN
-		// 			LET l_ccnt = l_ccnt + 1;
-		// 			RETURN l_dtabname, l_dcolname, l_ptabname, l_pcolname, l_constrname WITH RESUME;
-		// 		END IF
-		// 		END IF
-		// 	END FOREACH;
-
-		// 	FOREACH
-
-		// 		SELECT  dc.constrname,
-		// 				TRIM(dt.tabname), dt.tabid, di.indexkeys,
-		// 				TRIM(pt.tabname), pt.tabid, pi.indexkeys
-		// 		INTO    l_constrname,
-		// 				l_dtabname, l_dtabid, l_dindexkeys,
-		// 				l_ptabname, l_ptabid, l_pindexkeys
-		// 		FROM    systables      AS dt
-		// 		LEFT JOIN    sysconstraints AS dc ON dc.tabid    = dt.tabid
-		// 		LEFT JOIN    sysobjstate    AS do ON do.name     = dc.constrname
-		// 		LEFT JOIN    sysindices     AS di ON di.idxname  = dc.idxname
-		// 		LEFT JOIN    sysreferences  AS dr ON dr.constrid = dc.constrid
-		// 		LEFT JOIN    sysconstraints AS pc ON pc.constrid = dr.primary
-		// 		JOIN    systables      AS pt ON pt.tabid    = pc.tabid
-		// 		LEFT JOIN    sysindices     AS pi ON pi.idxname  = pc.idxname
-		// 		WHERE dt.tabid = t_tabid
-
-		// 		LET l_keyid = 2;
-
-		// 		LET l_dcolno = ikeyextractcolno(l_dindexkeys, l_keyid);
-		// 		LET l_pcolno = ikeyextractcolno(l_pindexkeys, l_keyid);
-
-		// 		IF l_dcolno != 0 AND l_dcolno == t_colno THEN
-
-		// 		SELECT  TRIM(colname)
-		// 		INTO    l_dcolname
-		// 		FROM    syscolumns
-		// 		WHERE   tabid = l_dtabid
-		// 		AND     colno = l_dcolno;
-
-		// 		SELECT  TRIM(colname)
-		// 		INTO    l_pcolname
-		// 		FROM    syscolumns
-		// 		WHERE   tabid = l_ptabid
-		// 		AND     colno = l_pcolno;
-
-		// 		IF l_dcolname == t_colname THEN
-		// 			LET l_ccnt = l_ccnt + 1;
-		// 			RETURN l_dtabname, l_dcolname, l_ptabname, l_pcolname, l_constrname WITH RESUME;
-		// 		END IF
-		// 		END IF
-		// 	END FOREACH;
-
-		// 	IF l_ccnt == 0 THEN
-		// 		RETURN t_tabname, t_colname, '', '', '' WITH RESUME;
-		// 	END IF
-
-		// 	END FOREACH;
-
-		// END FUNCTION;
-		// `;
-
-		// this.knex.raw(sp_allconst);
-
-		// let query = `
-		// SELECT table as TABLE_NAME,
-		// column as COLUMN_NAME,
-		// get_column_default( st.tabname, sc.colname ) as COLUMN_DEFAULT,
-		// CASE
-		// WHEN is_primary_key( st.tabname, sc.colname ) == 1 THEN 'PRI'
-		// WHEN is_primary_key( st.tabname, sc.colname ) == 0 THEN ''
-		// END AS COLUMN_KEY,
-		// CASE
-		// WHEN (sc.coltype = 2) THEN 'int(11)'
-		// WHEN (sc.coltype = 3) THEN 'float'
-		// WHEN (sc.coltype = 5) THEN 'decimal'
-		// WHEN (sc.coltype = 7) THEN 'date'
-		// WHEN (sc.coltype = 8) THEN 'money'
-		// WHEN (sc.coltype = 10) THEN 'datetime'
-		// WHEN (sc.coltype = 15) THEN CONCAT( CONCAT('char(', TO_CHAR(sc.collength)), ')')
-		// WHEN (sc.coltype = 16) THEN CONCAT( CONCAT('char(', TO_CHAR(sc.collength)), ')')
-		// WHEN (sc.coltype = 40) THEN CONCAT( CONCAT('char(', TO_CHAR(sc.collength)), ')')
-		// WHEN (sc.coltype = 256) THEN CONCAT( CONCAT('char(', TO_CHAR(sc.collength)), ')')
-		// WHEN (sc.coltype = 258) THEN 'int(11)'
-		// WHEN (sc.coltype = 262) THEN 'int(11)'
-		// WHEN (sc.coltype = 263) THEN 'date'
-		// WHEN (sc.coltype = 266) THEN 'datetime'
-		// WHEN (sc.coltype = 271) THEN CONCAT( CONCAT('char(', TO_CHAR(sc.collength)), ')')
-		// WHEN (sc.coltype = 272) THEN CONCAT( CONCAT('char(', TO_CHAR(sc.collength)), ')')
-		// END AS COLUMN_TYPE,
-		// CASE
-		// WHEN (sc.coltype = 2) THEN 10
-		// WHEN (sc.coltype = 3) THEN 10
-		// WHEN (sc.coltype = 5) THEN 10
-		// WHEN (sc.coltype = 7) THEN 30
-		// WHEN (sc.coltype = 8) THEN 10
-		// WHEN (sc.coltype = 10) THEN 30
-		// WHEN (sc.coltype = 15) THEN sc.collength
-		// WHEN (sc.coltype = 16) THEN sc.collength
-		// WHEN (sc.coltype = 40) THEN sc.collength
-		// WHEN (sc.coltype = 256) THEN sc.collength
-		// WHEN (sc.coltype = 258) THEN 10
-		// WHEN (sc.coltype = 262) THEN 10
-		// WHEN (sc.coltype = 263) THEN 30
-		// WHEN (sc.coltype = 266) THEN 30
-		// WHEN (sc.coltype = 271) THEN sc.collength
-		// WHEN (sc.coltype = 272) THEN sc.collength
-		// END AS CHARCTER_MAXIMUM_LENGTH,
-		// CASE
-		// WHEN (sc.coltype = 2) THEN 'YES'
-		// WHEN (sc.coltype = 3) THEN 'YES'
-		// WHEN (sc.coltype = 5) THEN 'YES'
-		// WHEN (sc.coltype = 7) THEN 'YES'
-		// WHEN (sc.coltype = 8) THEN 'YES'
-		// WHEN (sc.coltype = 10) THEN 'YES'
-		// WHEN (sc.coltype = 15) THEN 'YES'
-		// WHEN (sc.coltype = 16) THEN 'YES'
-		// WHEN (sc.coltype = 40) THEN 'YES'
-		// WHEN (sc.coltype = 256) THEN 'NO'
-		// WHEN (sc.coltype = 258) THEN 'NO'
-		// WHEN (sc.coltype = 262) THEN 'NO'
-		// WHEN (sc.coltype = 263) THEN 'NO'
-		// WHEN (sc.coltype = 266) THEN 'NO'
-		// WHEN (sc.coltype = 271) THEN 'NO'
-		// WHEN (sc.coltype = 272) THEN 'NO'
-		// END AS IS_NULLABLE,
-		// CASE
-		// WHEN is_primary_key( st.tabname, sc.colname ) == 1 THEN 'PRI'
-		// WHEN is_primary_key( st.tabname, sc.colname ) == 0 THEN ''
-		// END AS COLUMN_KEY,
-		// '' as EXTRA,
-		// 'NULL' as COLLATION_NAME,
-		// '' as COLUMN_COMMENT,
-		// sc.collength as NUMERIC_PRECISION,
-		// 'NULL' as GENERATION_EXPRESSION,
-		// 0 as NUMERIC_SCALE,
-		// foreign_key_table as REFERENCED_TABLE_NAME,
-		// foreign_key_column as REFERENCED_COLUMN_NAME, constraint_name,
-		// 'RESTRICT' as update_rule, 'RESTRICT' as delete_rule,
-		// 'NONE' as MATCH_OPTION
-		// FROM syscolumns sc, systables st, TABLE (FUNCTION sp_allinfo())
-		// allcinfo( table, column, foreign_key_table, foreign_key_column, constraint_name)
-		// WHERE st.tabname = table and sc.tabid = st.tabid and sc.colname = column
-		// `;
+		this.knex.raw(isPrimaryKey);
 
 		let query = `
 		SELECT table as TABLE_NAME,
@@ -796,7 +475,10 @@ export default class InformixDB implements SchemaInspector {
 			WHEN (sc.coltype = 271) THEN 'NO'
 			WHEN (sc.coltype = 272) THEN 'NO'
 		END AS IS_NULLABLE,
-		'PRI' as COLUMN_KEY,
+		CASE
+			WHEN is_primary_key( st.tabname, sc.colname ) == 1 THEN 'PRI'
+			WHEN is_primary_key( st.tabname, sc.colname ) == 0 THEN ''
+	  	END AS COLUMN_KEY,
 		'' as EXTRA,
 		'NULL' as COLLATION_NAME,
 		'' as COLUMN_COMMENT,
@@ -822,12 +504,10 @@ export default class InformixDB implements SchemaInspector {
 			query += `
 				and column = '${column}'
 			`;
-			// AND column = '${column}' ORDER BY 1,14,15
+
 			const rawColumn: RawColumn = await this.knex.raw(query);
 			return rawColumnToColumn(rawColumn);
 		}
-
-		// query += 'ORDER BY 1,14,15';
 
 		const res: RawColumn[][] = await this.knex.raw(query);
 		const records: RawColumn[] = res[0];
