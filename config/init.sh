3c9359c4a21821005f9dc1904c428e1b83791d66
nangelov77 <nikolay.angelov@etisoftware.com>
2024-07-22 14:59:30 +0300

    fix: allow directus to connect to the informix connection manager

--- config/init.sh
+++ config/init.sh
@@ -10,4 +10,4 @@ sed -i "s/__DB_PORT__/${DB_PORT}/g" /etc/odbc.ini
 
 sed -i "s/__DB_SERVER__/${DB_SERVER}/g" /opt/IBM/informix/4.10/etc/sqlhosts
 sed -i "s/__DB_HOST__/${DB_HOST}/g" /opt/IBM/informix/4.10/etc/sqlhosts
-sed -i "s/__INFORMIX_SERVER__/${INFORMIXSERVER}/g" /opt/IBM/informix/4.10/etc/sqlhosts
+sed -i "s/__DB_PORT__/${DB_PORT}/g" /opt/IBM/informix/4.10/etc/sqlhosts
